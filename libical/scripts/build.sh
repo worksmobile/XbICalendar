68e16cc33d6ca8d081f26c97fbc62942daee59ea
intaek.cho <intaek.cho@navercorp.com>
2024-02-07 18:22:19 +0900

    arm64 지원 - 스크립트 수정
    - https://wiki.navercorp.com/pages/viewpage.action?pageId=1052894226 참고
    - 깃헙 clone에 문제가 있어 수정
    - cmake 사용에 문제가 있어 full path 사용

--- libical/scripts/build.sh
+++ libical/scripts/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -o xtrace
 
 # Change Working Directory to build
@@ -16,41 +15,37 @@ echo "SCRIPTS_DIR : $SCRIPTS_DIR"
 echo "OUTPUT_DIR  : $OUTPUT_DIR"
 LIPO="xcrun -sdk iphoneos lipo"
 
-#
 # Verify Tools Are Installed
 command -v cmake >/dev/null 2>&1 || { echo >&2 "cmake required but it's not installed.  Aborting."; exit 1; }
 
-#
 # Download the latest library
-LIBRARY_VISION="v1.0.1"
+LIBRARY_BRANCH="test/system"
 LIBRARY_TARBALL="libical"
 if [ ! -d  ./$LIBRARY_TARBALL ]; then
-  LIBRARY_DISTRO_URL="https://github.com/libical/libical"
-
-  git clone $LIBRARY_DISTRO_URL
-  git checkout tags/$LIBRARY_VISION > /dev.null
-
+  LIBRARY_DISTRO_URL="https://github.com/worksmobile/libical.git"
+  git clone --branch $LIBRARY_BRANCH $LIBRARY_DISTRO_URL
 fi
-
 
 LIBRARY_DIR="libical"
 pushd $LIBRARY_DIR > /dev/null
 
-
-archList=( armv7 armv7s arm64 i386 x86_64 )
-for AA in "${archList[@]}"
-do
-    ARCH="$AA" $SCRIPTS_DIR/libical_make.sh
-done
-
-# build up fat library
+ARCH="arm64" $SCRIPTS_DIR/libical_make.sh device
+ARCH="arm64" $SCRIPTS_DIR/libical_make.sh sim
+ARCH="x86_64" $SCRIPTS_DIR/libical_make.sh sim
+rm -rf $OUTPUT_DIR/sim-lib
+rm -rf $OUTPUT_DIR/device-lib
+mkdir -p $OUTPUT_DIR/sim-lib
+mkdir -p $OUTPUT_DIR/device-lib
+cp -f $OUTPUT_DIR/arm64_device/libical.a $OUTPUT_DIR/device-lib
 $LIPO \
-    -arch arm64  $OUTPUT_DIR/arm64/libical.a \
-    -arch armv7  $OUTPUT_DIR/armv7/libical.a \
-    -arch armv7s $OUTPUT_DIR/armv7s/libical.a \
-    -arch x86_64 $OUTPUT_DIR/x86_64/libical.a \
-    -arch i386 $OUTPUT_DIR/i386/libical.a \
-    -create -output $OUTPUT_DIR/libical.a
+    -arch arm64  $OUTPUT_DIR/arm64_sim/libical.a \
+    -arch x86_64 $OUTPUT_DIR/x86_64_sim/libical.a \
+    -create -output $OUTPUT_DIR/sim-lib/libical.a
+xcodebuild -create-xcframework \
+    -library $OUTPUT_DIR/device-lib/libical.a \
+    -library $OUTPUT_DIR/sim-lib/libical.a \
+    -headers $OUTPUT_DIR/../../src/include \
+    -output $OUTPUT_DIR/ical.xcframework
 
 # make zoneinfo directory
 pushd build/zoneinfo
@@ -59,7 +54,7 @@ popd  >/dev/null
 
 # Move things to their final place
 mkdir -p ../../lib
-cp -f $OUTPUT_DIR/libical.a ../../lib
+cp -f $OUTPUT_DIR/ical.xcframework ../../lib
 mkdir -p ../../src/include
 cp  -f ./build/src/libical/ical.h ../../src/include
 cp  -f ./build/src/libical_ical_export.h ../../src/include
@@ -70,4 +65,3 @@ mv  $OUTPUT_DIR/share/libical/zoneinfo ../../zoneinfo
 popd  > /dev/null
 
 exit
-
