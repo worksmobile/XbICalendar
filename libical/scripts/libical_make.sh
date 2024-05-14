1559a1cf9fffec58e369a1a8dd27c23fe8397ab0
intaek.cho <intaek.cho@navercorp.com>
2024-05-14 13:23:31 +0900

    arm64 지원 - 스크립트 수정
    - https://wiki.navercorp.com/pages/viewpage.action?pageId=1052894226 참고
    - 깃헙 clone에 문제가 있어 수정
    	- 최신 태그를 바라보도록 수정
    - cmake 사용에 문제가 있어 full path 사용
    - 최종 결과 파트 수정
    	- 만들어진 결과가 제대로 복사되고 있지 않아 명령어 수정
    	- XCFramework 강제 이동 및 libical_ical_export.h의 정확한 위치 지정

--- libical/scripts/libical_make.sh
+++ libical/scripts/libical_make.sh
@@ -1,66 +1,38 @@
 #!/bin/sh
-
 set -o xtrace
 
 # SEE: http://www.smallsharptools.com/downloads/libical/
-
 PATH="`xcode-select -print-path`:/usr/bin:/usr/bin:/bin:/usr/local/bin"
 
-
 if [ ! -n "$ARCH" ]; then
-    export ARCH=armv7
+    export ARCH=arm64
 fi
-
 
 # Select the desired iPhone SDK
-export SDKVER="9.1"
+export SDKVER=""
 export DEVROOT=`xcode-select --print-path`
+export XML_CATALOG_FILES=/usr/local/etc/xml/catalog
 
-if [ "i386" = $ARCH ] || [ "x86_64" = $ARCH ]; then
-    export SDKROOT=$DEVROOT/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator${SDKVER}.sdk
-    export MIOS="-mios-simulator-version-min=7.1"
+if [ "$1" = "sim" ]; then
+        export SDKROOT=$DEVROOT/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator${SDKVER}.sdk
+        export MIOS="-mios-simulator-version-min=15.0"
+        export IOSROOT=$DEVROOT/Platforms/iPhoneSimulator.platform
+        export CPPFLAGS="-arch $ARCH -I$SDKROOT/usr/include $MIOS --debug"
+        export CFLAGS="$CPPFLAGS -pipe -no-cpp-precomp -isysroot $SDKROOT -target $ARCH-apple-ios-simulator"
+elif [ "$1" = "device" ]; then
+        export SDKROOT=$DEVROOT/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${SDKVER}.sdk
+        export MIOS="-miphoneos-version-min=15.0"
+        export IOSROOT=$DEVROOT/Platforms/iPhoneOS.platform
+        export CPPFLAGS="-arch $ARCH -I$SDKROOT/usr/include $MIOS --debug"
+        export CFLAGS="$CPPFLAGS -pipe -no-cpp-precomp -isysroot $SDKROOT -target $ARCH-apple-ios"
 else
-    export SDKROOT=$DEVROOT/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${SDKVER}.sdk
-    export MIOS="-miphoneos-version-min=7.1"
-fi;
-export IOSROOT=$DEVROOT/Platforms/iPhoneOS.platform
-
-
-
-if [ ! -d $DEVROOT ]
-then
-        echo "Developer Root not found! - $DEVROOT"
         exit 1
 fi
-
-echo "DEVROOT = $DEVROOT"
-
-if [ ! -d $SDKROOT ]
-then
-        echo "SDK Root not found! - $SDKROOT"
-        exit 1
-fi
-
-echo "SDKROOT = $SDKROOT"
-
-if [ ! -d $IOSROOT ]
-then
-        echo "iOS Root not found! - $IOSROOT"
-        exit 1
-fi
-
+# Set up relevant environment variables
 echo "IOSROOT = $IOSROOT"
-
-# Set up relevant environment variables 
-export CPPFLAGS="-arch $ARCH -I$SDKROOT/usr/include $MIOS --debug"
-export CFLAGS="$CPPFLAGS -pipe -no-cpp-precomp -isysroot $SDKROOT "
 export CXXFLAGS="$CFLAGS"
-
 export LDFLAGS="-L$SDKROOT/usr/lib/ -arch $ARCH"
-
 export CLANG=$DEVROOT/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
-
-
 export CC=$CLANG
 export CXX=$CLANG++
 export LD=$DEVROOT/usr/bin/ld
@@ -72,48 +44,55 @@ export RANLIB=$DEVROOT/usr/bin/ranlib
 export LIPO="xcrun -sdk iphoneos lipo"
 export HOST=arm-apple-darwin10
 
+if [ ! -d $DEVROOT ]
+then
+        echo "Developer Root not found! - $DEVROOT"
+        exit 1
+fi
+echo "DEVROOT = $DEVROOT"
+if [ ! -d $SDKROOT ]
+then
+        echo "SDK Root not found! - $SDKROOT"
+        exit 1
+fi
+echo "SDKROOT = $SDKROOT"
+if [ ! -d $IOSROOT ]
+then
+        echo "iOS Root not found! - $IOSROOT"
+        exit 1
+fi
+
 if [ ! -f $CC ]
 then
         echo "C Compiler not found! - $CC"
         exit 1
 fi
-
 if [ ! -f $CXX ]
 then
         echo "C++ Compiler not found! - $CXX"
         exit 1
 fi
-
 if [ ! -f $LD ]
 then
         echo "Linker not found! - $LD"
         exit 1
 fi
-
 if [ -d $OUTPUT_DIR/$ARCH ]
-then 
+then
        rm -rf $OUTPUT_DIR/$ARCH
 fi
-
 find ./src -name \*.a -exec rm {} \;
-
 
 mkdir build
 cd build
 rm -rf *
-cmake -DCMAKE_OSX_ARCHITECTURES=$ARCH -DUSE_BUILTIN_TZDATA=true -DCMAKE_INSTALL_PREFIX="$OUTPUT_DIR" ..
-
+/opt/homebrew/bin/cmake -DCMAKE_OSX_ARCHITECTURES=$ARCH -DUSE_BUILTIN_TZDATA=true -DCMAKE_INSTALL_PREFIX="$OUTPUT_DIR" ..
 make
 
 # copy the files to the arch folder
-
 mkdir -p $OUTPUT_DIR
-mkdir -p $OUTPUT_DIR/$ARCH
-
-cp `find . -name \*.a` $OUTPUT_DIR/$ARCH/
-
+mkdir -p $OUTPUT_DIR/"$ARCH"_"$1"
+cp `find . -name \*.a` $OUTPUT_DIR/"$ARCH"_"$1"/
 $LIPO -info $OUTPUT_DIR/$ARCH/*.a
-
 echo $ARCH DONE
-
 echo "See $OUTPUT_DIR"
